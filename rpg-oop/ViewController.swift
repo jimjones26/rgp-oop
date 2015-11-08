b87dc9dda289f9085dd8450c19654528d7dece13
Jimmy Jones <jimjones26@gmail.com>
2015-11-07 23:14:47 -0500

    add game logic, update UI for attack button and visibility based on state

--- rpg-oop/ViewController.swift
+++ rpg-oop/ViewController.swift
@@ -10,20 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var player: Player!
+    var enemy: Enemy!
+    var chestMessage: String?
+    
     @IBOutlet weak var printLbl: UILabel!
     @IBOutlet weak var playerHpLbl: UILabel!
     @IBOutlet weak var enemyHpLbl: UILabel!
     @IBOutlet weak var enemyImg: UIImageView!
     @IBOutlet weak var chestBtn: UIButton!
+    @IBOutlet weak var attackBtn: UIButton!
     
     @IBAction func onChestTapped(sender: AnyObject) {
+        chestBtn.hidden = true
+        printLbl.text = chestMessage
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "generateRandomEnemy", userInfo: nil, repeats: false)
+    }
+    
+    @IBAction func attackTapped(sender: AnyObject) {
         
+        if enemy.attemptAttack(player.attackPwr) {
+            printLbl.text = "Attacked \(enemy.type) for \(player.attackPwr) HP"
+            enemyHpLbl.text = "\(enemy.hp) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful!"
+        }
+        
+        if let loot = enemy.dropLoop() {
+            player.addItemToInventory(loot)
+            chestMessage = "\(player.name) found \(loot)"
+            chestBtn.hidden = false
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLbl.text = ""
+            printLbl.text = "Killed \(enemy.type)"
+            enemyImg.hidden = true
+            attackBtn.enabled = false
+        }
+    }
+    
+    
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 15)
+        }
+        
+        enemyImg.hidden = false
+        enemyHpLbl.text = "\(enemy.hp) HP"
+        attackBtn.enabled = true
     }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        player = Player(name: "DirtyLaundery21", hp: 110, attackPwr: 20)
+        
+        generateRandomEnemy()
+        
+        playerHpLbl.text = "\(player.hp) HP"
+        enemyHpLbl.text = "\(enemy.hp) HP"
+        
     }
 
     override func didReceiveMemoryWarning() {
